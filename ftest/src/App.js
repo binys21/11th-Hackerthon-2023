55431bbfa6bbcfad5ae759c3fee125ff497de028
jiwon <won0805@ewhain.net>
2023-08-04 02:46:59 +0900

    회원가입 첫 창

--- ftest/src/App.js
+++ ftest/src/App.js
@@ -10,7 +10,7 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<LoginPage />} />
-                    <Route path="/signup" element={<LoginPage />} />
+                    <Route path="/signUp" element={<SignUpPage />} />
                 </Routes>
             </Router>
         </>
---
+++ ftest/src/pages/Login/SignUpPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import BfVerify from "../../components/Login_comp/BfVerify";
+
+const SignUpPage = () => {
+    return <BfVerify />;
+};
+
+export default SignUpPage;
