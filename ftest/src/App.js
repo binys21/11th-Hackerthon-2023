7f889ddc837fa53c0db7e0cc34617526cc3cd6b1
jiwon <won0805@ewhain.net>
2023-08-08 16:46:39 +0900

    signUp 수정

--- ftest/src/App.js
+++ ftest/src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/Login/LoginPage";
 import SignUpPage from "./pages/Login/SignUpPage";
-import MyHistory from './pages/Patient/MyHistory';
-import HistoryDetail from './pages/Patient/HistoryDetail';
-import DrugDetail from './pages/Patient/DrugDetail';
-import CategoryPage from './pages/CategoryPage';
-import SurgeryDetail from './pages/Patient/SurgeryDetail';
+import MyHistory from "./pages/Patient/MyHistory";
+import HistoryDetail from "./pages/Patient/HistoryDetail";
+import DrugDetail from "./pages/Patient/DrugDetail";
+import CategoryPage from "./pages/CategoryPage";
+import SurgeryDetail from "./pages/Patient/SurgeryDetail";
 
 function App() {
     return (
@@ -15,12 +15,12 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<LoginPage />} />
-                    <Route path="/signup" element={<LoginPage />} />
-                    <Route path="/myhistory" element={<MyHistory/>}/>
-                    <Route path="/historydetail" element={<HistoryDetail/>}/>
-                    <Route path="/drugdetail" element={<DrugDetail/>}/>
-                    <Route path="/category" element={<CategoryPage/>}/>
-                    <Route path="/surgerydetail" element={<SurgeryDetail/>}/>
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/myhistory" element={<MyHistory />} />
+                    <Route path="/historydetail" element={<HistoryDetail />} />
+                    <Route path="/drugdetail" element={<DrugDetail />} />
+                    <Route path="/category" element={<CategoryPage />} />
+                    <Route path="/surgerydetail" element={<SurgeryDetail />} />
                 </Routes>
             </Router>
         </>
