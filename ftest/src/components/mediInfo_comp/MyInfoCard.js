5659e27beb37475336b7f432dbddbd5a678d001f
jiwon <won0805@ewhain.net>
2023-08-12 01:38:05 +0900

    의료정보 조회 css 수정완료(블러제외)

--- ftest/src/components/mediInfo_comp/MyInfoCard.js
+++ ftest/src/components/mediInfo_comp/MyInfoCard.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 //image
-import lowbtn from '../../pages/images/lowbtn.png';
+import lowbtn from "../../pages/images/lowbtn.png";
 
-const MyInfoCard=()=>{
+const MyInfoCard = () => {
     const [isBlur, setIsBlur] = useState(true);
     const [isButtonVisible, setIsButtonVisible] = useState(true);
 
@@ -12,99 +12,121 @@ const MyInfoCard=()=>{
         setIsButtonVisible(!isButtonVisible);
     };
 
-    return(
+    return (
         <>
-        {isButtonVisible && (
-                        <BtnWrapper>
-                        <button onClick={toggleBlur}>
-                                 내려서 상세보기
-                                 <br></br>
-                                 <br></br>
-                                <img src={lowbtn} alt=""></img>
-                            </button>
-                        </BtnWrapper>
-        )}
+            {isButtonVisible && (
+                <BtnWrapper>
+                    <button onClick={toggleBlur}>
+                        내려서 상세보기
+                        <br></br>
+                        <br></br>
+                        <img src={lowbtn} alt=""></img>
+                    </button>
+                </BtnWrapper>
+            )}
 
-
-         <BoxWrapper blur={isBlur}>
-
-            <ContentWrapper>
-                <NameWrapper>정대만</NameWrapper>
-                남/2000.05.22
-                <br></br>
-                000522-3121212
-                <br></br>
-                A형/Rh+
-                <br></br>
-                184cm/70kg
-                <br></br>
-                000-0000-0000
-                <br></br>
-                서울특별시 서대문구 성산로 508
-
-            </ContentWrapper>
-            <TitleWrapper>
-                이름
-                <br></br>
-                성별/생년월일
-                <br></br>
-                주민번호
-                <br></br>
-                혈액형
-                <br></br>
-                키/체중
-                <br></br>
-                연락처
-                <br></br>
-                거주지
-            </TitleWrapper>
-        </BoxWrapper>
+            <BoxWrapper blur={isBlur}>
+                <ContentWrapper>
+                    <NameWrapper>정대만</NameWrapper>
+                    남/2000.05.22
+                    <br></br>
+                    000522-3121212
+                    <br></br>
+                    A형/Rh+
+                    <br></br>
+                    184cm/70kg
+                    <br></br>
+                    000-0000-0000
+                    <br></br>
+                    서울특별시 서대문구 성산로 508
+                </ContentWrapper>
+                <TitleWrapper>
+                    이름
+                    <br></br>
+                    성별/생년월일
+                    <br></br>
+                    주민번호
+                    <br></br>
+                    혈액형
+                    <br></br>
+                    키/체중
+                    <br></br>
+                    연락처
+                    <br></br>
+                    거주지
+                </TitleWrapper>
+            </BoxWrapper>
         </>
     );
-
 };
 export default MyInfoCard;
-const BoxWrapper=styled.div`
 
-filter: ${({ blur }) => (blur ? "blur(1px)" : "none")};
--webkit-filter: ${({ blur }) => (blur ? "blur(5px)" : "none")};
+// const BtnWrapper = styled.div`
+//     filter: blur(10px);
+//     -webkit-filter: blur(10px);
 
-display: flex;
-background-color:#175DF9;
-width:333px;
-height:315px;
-color:white;
-border-radius:10px;
+//     position: absolute;
+//     top: 25%;
+//     width: 87%;
+//     height: 250px;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: flex-start;
+//     align-items: center;
+//     z-index: 9;
+//     border-radius: 10px;
+
+//     div {
+//         color: white;
+//         margin: 10px 0;
+//         z-index: 10;
+//     }
+//     img {
+//         width: 20px;
+//         height: 10px;
+//         z-index: 10;
+//     }
+// `;
+const BtnWrapper = styled.div`
+    button {
+        color: white;
+        border: 0;
+        background-color: transparent;
+    }
+    z-index: 100;
+    position: absolute;
+    top: 20%;
 `;
 
-const TitleWrapper=styled.div`
-font-size:12px;
-font-weight:400;
-line-height:3.4;
-text-align:right;
-margin-top:13px;
-margin-left:15px;
+const BoxWrapper = styled.div`
+    filter: ${({ blur }) => (blur ? "blur(1px)" : "none")};
+    -webkit-filter: ${({ blur }) => (blur ? "blur(5px)" : "none")};
+
+    display: flex;
+    background-color: #175df9;
+    width: 100%;
+    border-radius: 10px;
+    margin-bottom: 40px;
+    color: white;
 `;
-const NameWrapper=styled.div`
-font-size:24px;
-font-weight:600;
-margin-top:4px;
+const TitleWrapper = styled.div`
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 3.2;
+    text-align: right;
+    margin-top: 15px;
 `;
-const ContentWrapper=styled.div`
-font-size:16px;
-font-weight:700;
-line-height:2.5;
-margin-left:20px;
+const NameWrapper = styled.div`
+    font-size: 22px;
+    font-weight: 600;
+    margin-top: 4px;
+    text-align: left;
 `;
-const BtnWrapper=styled.div`
-button {
-    color:white;
-    border: 0;
-    background-color: transparent;
-    
-}
-z-index:100;
-position: absolute;
-top:20%;
-left: 40%;
+const ContentWrapper = styled.div`
+    font-size: 15px;
+    font-weight: 700;
+    line-height: 2.5;
+    margin-left: 18px;
+    text-align: left;
+    padding-bottom: 15px;
 `;
