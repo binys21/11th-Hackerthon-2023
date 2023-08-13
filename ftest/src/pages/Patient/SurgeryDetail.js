248737244b64ffb5aad5bbe9c51478fa06fbeafd
binys21 <binys1121@ewhain.net>
2023-08-14 01:11:21 +0900

    detail component 수정 & 수술 추가

--- ftest/src/pages/Patient/SurgeryDetail.js
+++ ftest/src/pages/Patient/SurgeryDetail.js
@@ -15,7 +15,6 @@ const SurgeryDetail=()=>{
         <>
         <Wrapper>
             <Header title="상세보기"/>
-
         <Container>
         <InfoWrapper>
         차트 No.038415                                                 2023.06.13 | 최영아 작성
@@ -26,8 +25,55 @@ const SurgeryDetail=()=>{
             addr: "서울 서대문구 연세로 36 독수리빌딩",
             doctor: "권한민 약사" }} />
 
-        <PatientBox></PatientBox>
-        <SurgeryBox></SurgeryBox>
+        <PatientBox>
+            <div className='date'>2023.06.03 수술 | 1차 수술 
+            연세세브란스
+            </div>
+            <div className='reginum'>환자 등록번호 : 0000193</div>
+
+            <TitleWrapper>
+                <div className='thick'>환자의 정보</div>
+                <hr></hr>
+                환자의 성명
+                <br></br>
+                환자의 주민등록번호
+                <hr></hr>
+                나이
+                <br></br>
+                성별
+                <br></br>
+                연락처
+            </TitleWrapper>
+
+            <PaientInfo>
+                정대만
+                <br></br>
+                720922-1234567
+                <hr></hr>
+                만 17세
+                <br></br>
+                남
+                <br></br>
+                010-2408-3893
+            </PaientInfo>
+
+        </PatientBox>
+        <SurgeryBox>
+            해당 수술과
+            <hr></hr>
+            수술 집도의명 <br></br> 수술 보조의명 
+            <br></br><br></br>  <hr></hr>
+            마취의명
+            <DocterWrapper>
+            비뇨의학과 <br></br>
+            김현아 의사 <br></br>
+            김무열 의사 <br></br>
+            김원필 간호사 <br></br><br></br>
+            고이찬 의사 
+        </DocterWrapper>
+        </SurgeryBox>
+
+
 
         </Container>
         </Wrapper>
@@ -71,4 +117,13 @@ background-color: white;
 width: 333px;
 height: 174px;
 border-radius: 9.63px;
+`;
+const TitleWrapper=styled.div`
+`;
+const PaientInfo=styled.div`
+`;
+const DocterWrapper=styled.div`
+position:absolute;
+top:62%;
+left:70%;
 `;
