253893611a9a736b33a03c9095aca7e266d54f08
binys21 <binys1121@ewhain.net>
2023-08-14 14:18:58 +0900

    내가 남긴 질문

---
+++ ftest/src/components/qna_comp/QuestionCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const QuestionCard = () => {
+
+
+    return (
+        <>
+            <Wrapper>
+                <BoxWrapper>
+ 
+                </BoxWrapper>
+            </Wrapper>
+        </>
+    );
+};
+
+export default QuestionCard;
+const Wrapper = styled.div`
+    background-color: none;
+`;
+const BoxWrapper = styled.div`
+width: 343px;
+height: 127px;
+flex-shrink: 0;
+border-radius: 10px;
+border: 1px solid rgba(255, 255, 255, 0.05);
+background: #363B46;
+margin-bottom:20px;
+`;
+
